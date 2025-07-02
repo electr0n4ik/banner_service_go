0e145e2721f656c60003d189656c2a1051235782
electr0n4ik <andreyshka3@gmail.com>
2025-07-02 15:19:33 +0300

    Add handle in HelloHandler

--- internal/delivery/handler.go
+++ internal/delivery/handler.go
@@ -1,10 +1,13 @@
 package deliveryhttp
 
 import (
+	"log"
 	"net/http"
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("Hello World!"))
+	if _, err := w.Write([]byte("Hello World!")); err != nil {
+		log.Fatalf("Ошибка записи в ответ: %s", err)
+	}
 }
